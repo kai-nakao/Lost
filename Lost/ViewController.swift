92de9e2a9394f79f0fce833625a5df451636487e
kai.nakao <nkkai0803@gmail.com>
2020-10-16 14:06:18 +0900

    Initial Commit

---
+++ Lost/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lost
+//
+//  Created by 中尾開 on 2020/10/16.
+//  Copyright © 2020 kai.nakao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
