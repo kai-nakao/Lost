67bdea3a98ec18d7319b6ed0ecd4a27b4c1f01ec
kai-nakao <65479384+kai-nakao@users.noreply.github.com>
2020-11-16 10:31:43 +0900

    Update AppDelegate.swift

--- Lost/AppDelegate.swift
+++ Lost/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-        GMSServices.provideAPIKey("AIzaSyCRw9FckUuxVqtS0cU5kFEmY2MMwMSVGCg")
+        GMSServices.provideAPIKey("")
         return true
     }
 
